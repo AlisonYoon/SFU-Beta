e5c8e98d0cf181a878f021efab1136cc72fe3e11
Alison Yoon <politescarlett1@gmail.com>
2018-02-06 13:36:53 +0900

    external js file

---
+++ assets/js/beta_1.0.js
@@ -0,0 +1,46 @@
+function nameEdit() {
+  // document.getElementById("myName").readOnly = false;
+  document.getElementById("myName").select();
+  this.setSelectionRange(0, this.value.length);
+};
+function videoHeight() {
+console.error("ttttttt");
+var videoDom = document.getElementsByClassName("video-box");
+for(var i=0, n=videoDom.length; i<n; i++){
+  var videoWidth = videoDom[i].offsetWidth;
+  var videoHeight = videoWidth * 3/4;
+  if (n < 3) {
+    videoWidth = videoDom[i].offsetWidth;
+    videoHeight = videoWidth * 3/4;
+    videoDom[i].style.height == videoHeight + "px";
+  } else if (n === 3) {
+    videoDom[i].style.width = 31 + "%";
+    videoWidth;
+    videoHeight;
+    videoDom[i].style.height = videoHeight + "px";
+  }  else if (n === 4) {
+  var screenContainer = document.getElementById("screenContainer");
+  screenContainer.style.width = 65 + "%";
+  videoHeight = (1 / 2 * document.getElementById("screenContainer").offsetHeight) - 120;
+
+  videoWidth = videoHeight * 4 / 3;
+  videoDom[i].style.height = videoHeight + "px";
+  videoDom[i].style.width = videoWidth + "px";
+} else {
+  var screenContainer = document.getElementById("screenContainer");
+  screenContainer.style.width = 65 + "%";
+  videoHeight = (1 / 2 * document.getElementById("screenContainer").offsetHeight) - 120 ;
+
+  videoDom[i].style.width = 31 + "%";
+
+  videoWidth = videoDom[i].offsetWidth;
+  videoHeight = videoWidth * 3 / 4;
+  videoDom[i].style.height = videoHeight + "px";
+
+  console.error(document.getElementById("screenContainer").offsetHeight);
+  console.error(document.getElementById("screenContainer").offsetWidth);
+  };
+  videoDom[i].style.height = videoHeight + "px";
+  console.error('videoWidth = ' + videoWidth + "  Height = " + videoHeight);
+}
+};
