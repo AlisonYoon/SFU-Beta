6f214805ab55e049c7a7fe5598de8cd01a26d4e0
Alison Yoon <politescarlett1@gmail.com>
2018-02-13 13:24:23 +0900

    video call full screen - js

--- assets/js/video_call.js
+++ assets/js/video_call.js
@@ -1,50 +1,65 @@
+var isfullscreen = false;
 function nameEdit() {
   // document.getElementById("myName").readOnly = false;
   document.getElementById("myName").select();
   this.setSelectionRange(0, this.value.length);
 };
 function videoHeight() {
-console.error("ttttttt");
-var videoDom = document.getElementsByClassName("video-box");
-for(var i=0, n=videoDom.length; i<n; i++){
-  var videoWidth = videoDom[i].offsetWidth;
-  var videoHeight = videoWidth * 3/4;
-  if (n < 3) {
-    videoWidth = videoDom[i].offsetWidth;
-    videoHeight = videoWidth * 3/4;
-    videoDom[i].style.height == videoHeight + "px";
-  } else if (n === 3) {
-    videoDom[i].style.width = 31 + "%";
-    videoWidth;
-    videoHeight;
-    videoDom[i].style.height = videoHeight + "px";
-  }  else if (n === 4) {
-  var screenContainer = document.getElementById("screenContainer");
-  screenContainer.style.height = 80 + "%";
-  screenContainer.style.width = 65 + "%";
-  videoHeight = (1 / 2 * document.getElementById("screenContainer").offsetHeight) - 120;
+  if(isfullscreen){
+    var screenContainer = document.getElementById("screenContainer");
+    screenContainer.style.height = "";
+    screenContainer.style.width = "";
+    var videoDom = document.getElementsByClassName("video-box");
+    for(var i=0, n=videoDom.length; i<n; i++){
+      videoDom[i].style.height = "";
+      videoDom[i].style.width = "";
+    }
+    return;
+  }
+  else{
+    console.error("ttttttt");
+    var videoDom = document.getElementsByClassName("video-box");
+    for(var i=0, n=videoDom.length; i<n; i++){
+      var videoWidth = videoDom[i].offsetWidth;
+      var videoHeight = videoWidth * 3/4;
+      if (n < 3) {
+        videoWidth = videoDom[i].offsetWidth;
+        videoHeight = videoWidth * 3/4;
+        videoDom[i].style.height == videoHeight + "px";
+      } else if (n === 3) {
+        videoDom[i].style.width = 31 + "%";
+        videoWidth;
+        videoHeight;
+        videoDom[i].style.height = videoHeight + "px";
+      }  else if (n === 4) {
+      var screenContainer = document.getElementById("screenContainer");
+      screenContainer.style.height = 80 + "%";
+      screenContainer.style.width = 65 + "%";
+      videoHeight = (1 / 2 * document.getElementById("screenContainer").offsetHeight) - 120;
 
-  videoWidth = videoHeight * 4 / 3;
-  videoDom[i].style.height = videoHeight + "px";
-  videoDom[i].style.width = videoWidth + "px";
-} else {
-  var screenContainer = document.getElementById("screenContainer");
-  screenContainer.style.width = 65 + "%";
-  screenContainer.style.height = 80 + "%";
-  videoHeight = (1 / 2 * document.getElementById("screenContainer").offsetHeight) - 120 ;
+      videoWidth = videoHeight * 4 / 3;
+      videoDom[i].style.height = videoHeight + "px";
+      videoDom[i].style.width = videoWidth + "px";
+    } else {
+      var screenContainer = document.getElementById("screenContainer");
+      screenContainer.style.width = 65 + "%";
+      screenContainer.style.height = 80 + "%";
+      videoHeight = (1 / 2 * document.getElementById("screenContainer").offsetHeight) - 120 ;
 
-  videoDom[i].style.width = 31 + "%";
+      videoDom[i].style.width = 31 + "%";
 
-  videoWidth = videoDom[i].offsetWidth;
-  videoHeight = videoWidth * 3 / 4;
-  videoDom[i].style.height = videoHeight + "px";
+      videoWidth = videoDom[i].offsetWidth;
+      videoHeight = videoWidth * 3 / 4;
+      videoDom[i].style.height = videoHeight + "px";
 
-  console.error(document.getElementById("screenContainer").offsetHeight);
-  console.error(document.getElementById("screenContainer").offsetWidth);
-  };
-  videoDom[i].style.height = videoHeight + "px";
-  console.error('videoWidth = ' + videoWidth + "  Height = " + videoHeight);
-}
+      console.error(document.getElementById("screenContainer").offsetHeight);
+      console.error(document.getElementById("screenContainer").offsetWidth);
+      };
+      videoDom[i].style.height = videoHeight + "px";
+      console.error('videoWidth = ' + videoWidth + "  Height = " + videoHeight);
+    }
+  }
+
 };
 function openAttendee() {
    $("#roomActivity").toggle('open');
@@ -87,6 +102,7 @@ function toolMicOnOff() {
 function fullscreen(type){
 let element = document.getElementById("screenContainer");
 if(type===0){
+  isfullscreen = true;
 if (element.requestFullScreen) {
 element.requestFullScreen();
 }
@@ -97,7 +113,7 @@ else if(element.mozRequestFullScreen){
 element.mozRequestFullScreen();
 }
 $("#screenContainer").addClass('fullscreen');
-$(".video-box").addClass('fullscreen');
+$("#videoBox").addClass('fullscreen');
 $(".screenShareOwner ").addClass('fullscreen');
 $(".videoView-topToolbar button figure div").css('display','block');
 $(".videoView-topToolbar button figure img").css('display','none');
@@ -105,6 +121,7 @@ $(".video-toolbar-fixed").css('z-index', 10000000000);
 $(".video-space-header").css('z-index', 10000000000);
 }
 else{
+  isfullscreen = false;
 if (document.exitFullscreen){
 document.exitFullscreen();
 }
@@ -118,7 +135,7 @@ else if (document.webkitExitFullscreen){
 document.webkitExitFullscreen();
 }
 $("#screenContainer").removeClass('fullscreen');
-$(".video-box").removeClass('fullscreen');
+$("#videoBox").removeClass('fullscreen');
 $(".screenShareOwner ").removeClass('fullscreen');
 $(".videoView-topToolbar button figure div").css('display','none');
 $(".videoView-topToolbar button figure img").css('display','block');
