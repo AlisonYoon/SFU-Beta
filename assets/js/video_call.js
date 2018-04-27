da110168bf83aa83f91d75d5e2245fce75ac28f7
Alison Yoon <politescarlett1@gmail.com>
2018-04-27 13:17:54 +0900

    lobby user video screen , mic on/off added

--- assets/js/video_call.js
+++ assets/js/video_call.js
@@ -102,6 +102,7 @@ function toolVideoOnOff() {
   '-moz-box-shadow': 'none',
   'box-shadow': 'none'
   });
+      $('.user-video').css({'background':'transparent'});
     $('.cam-off-msg').hide();
   } else if ($('.video-btn').css('display')=='block') {
     $('.video-btn-off').show();
@@ -111,6 +112,7 @@ function toolVideoOnOff() {
     '-webkit-box-shadow': 'inset 0px 0px 153px 34px rgba(0,0,0,0.8)',
   '-moz-box-shadow': 'inset 0px 0px 153px 34px rgba(0,0,0,0.8)',
   'box-shadow': 'inset 0px 0px 153px 34px rgba(0,0,0,0.8)'});
+    $('.user-video').css({'background':'#000'});
   };
 };
 // toolVideoOnOff() changed for my video off screen
@@ -223,26 +225,3 @@ function settingsBox1() {
     //$('.settings-box').css('display','none');
   };
 };
-
-// New Lobby Video controls
-function lobbyVideoOnOff() {
-  console.log('clock');
-  // if ($('.lobby-video-btn').css('display')=='none') {
-  //   $('.lobby-video-btn-off').hide();
-  //   $('.lobby-video-btn').show();
-  //   $('.user-video').css({"background-color":"transparent",
-  //   '-webkit-box-shadow': 'none',
-  // '-moz-box-shadow': 'none',
-  // 'box-shadow': 'none'
-  // });
-  //   $('.cam-off-msg').hide();
-  // } else if ($('.lobby-video-btn').css('display')=='block') {
-  //   $('.video-btn-off').show();
-  //   $('.video-btn').hide();
-  //   $('.cam-off-msg').fadeTo('fast',1.00);
-  //   $('#videoFrame').css({"background-color":"rgba(0,0,0,.5)",
-  //   '-webkit-box-shadow': 'inset 0px 0px 153px 34px rgba(0,0,0,0.8)',
-  // '-moz-box-shadow': 'inset 0px 0px 153px 34px rgba(0,0,0,0.8)',
-  // 'box-shadow': 'inset 0px 0px 153px 34px rgba(0,0,0,0.8)'});
-  // };
-}
